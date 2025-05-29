de7a938287d7ff8db03823731b4e669fd323abff
m-pawlowicz <michal.pawlowicz.dev@gmail.com>
2025-05-29 19:02:42 +0200

    add synthetic loading state

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,12 +3,17 @@ import { useEffect, useState } from 'react';
 
 export function App() {
   const [users, setUsers] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch('/api/users')
       .then(res => res.json())
       .then(data => setUsers(data))
       .catch(err => console.error(err));
+
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000)
   }, []);
 
   console.log(users)
@@ -17,6 +22,7 @@ export function App() {
     <>
       <h1>Parcel React App</h1>
       <p>Edit <code>src/App.tsx</code> to get started!</p>
+      {loading ? <p>loading</p> : <ul>{users.map(x => <li>{x.name}</li>)}</ul>} 
     </>
   );
 }
